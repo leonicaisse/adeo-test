5f9cd329cb1af847c14b7bcab509b5792d0921f4
Léo Nicaisse <leo.nicaisse.partner@decathlon.com>
2024-09-05 15:30:44 +0200

    fix: linter configuration

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const { data } = require('./data')
-const { filterCountries } = require('./commands/filter')
-const { count } = require('./commands/count')
-const { serializeArguments, getArgumentValue } = require('./utils')
+const { data } = require('./data');
+const { filterCountries } = require('./commands/filter');
+const { count } = require('./commands/count');
+const { serializeArguments, getArgumentValue } = require('./utils');
 
 const main = () => {
-    const args = serializeArguments(process.argv.slice(2))
-    const commands = args.map((arg) => arg.name)
+   const args = serializeArguments(process.argv.slice(2));
+   const commands = args.map((arg) => arg.name);
 
-    if (commands.includes('filter')) {
-        const filter = getArgumentValue('filter', args)
-        console.log(JSON.stringify(filterCountries(data, filter), null, 2))
-    }
+   if (commands.includes('filter')) {
+      const filter = getArgumentValue('filter', args);
+      console.log(JSON.stringify(filterCountries(data, filter), null, 2));
+   }
 
-    if (commands.includes('count')) {
-        console.log(JSON.stringify(count(data), null, 2))
-    }
-}
+   if (commands.includes('count')) {
+      console.log(JSON.stringify(count(data), null, 2));
+   }
+};
 
-main()
+main();
 
 module.exports = {
-    main,
-}
+   main,
+};
--- commands/filter.js
+++ commands/filter.js
@@ -1,34 +1,34 @@
 const filterCountries = (countryList, filter) => {
-    const filterCountryList = countryList.reduce((list, country) => {
-        const filterPeopleList = filterPeople(country.people, filter)
-        if (filterPeopleList.length) {
-            list.push({ ...country, people: filterPeopleList })
-        }
-        return list
-    }, [])
-    return filterCountryList
-}
+   const filterCountryList = countryList.reduce((list, country) => {
+      const filterPeopleList = filterPeople(country.people, filter);
+      if (filterPeopleList.length) {
+         list.push({ ...country, people: filterPeopleList });
+      }
+      return list;
+   }, []);
+   return filterCountryList;
+};
 
 const filterPeople = (peopleList, filter) => {
-    const filteredPeopleList = peopleList.reduce((list, people) => {
-        const filteredAnimalList = filterAnimals(people.animals, filter)
-        if (filteredAnimalList.length) {
-            list.push({ ...people, animals: filteredAnimalList })
-        }
-        return list
-    }, [])
-    return filteredPeopleList
-}
+   const filteredPeopleList = peopleList.reduce((list, people) => {
+      const filteredAnimalList = filterAnimals(people.animals, filter);
+      if (filteredAnimalList.length) {
+         list.push({ ...people, animals: filteredAnimalList });
+      }
+      return list;
+   }, []);
+   return filteredPeopleList;
+};
 
 const filterAnimals = (animalList, filter) => {
-    const filteredAnimalList = animalList.filter((animal) =>
-        animal.name.includes(filter)
-    )
-    return filteredAnimalList
-}
+   const filteredAnimalList = animalList.filter((animal) =>
+      animal.name.includes(filter)
+   );
+   return filteredAnimalList;
+};
 
 module.exports = {
-    filterCountries,
-    filterPeople,
-    filterAnimals,
-}
+   filterCountries,
+   filterPeople,
+   filterAnimals,
+};

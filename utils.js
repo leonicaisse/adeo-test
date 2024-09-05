5fbd8bd76b8ef78a8073f41ba4e5f4f5a018f7b2
Léo Nicaisse <leo.nicaisse.partner@decathlon.com>
2024-09-05 16:21:35 +0200

    feat: improve error handling

--- utils.js
+++ utils.js
@@ -1,4 +1,8 @@
-const serializeArguments = (args) => {
+const serializeArguments = (args = []) => {
+   if (!args.length)
+      throw new Error(
+         'Please provide arguments, such as --count or --filter=<pattern>'
+      );
    return args.map((arg) => {
       arg = arg.slice(2);
       const [name, value] = arg.split('=');
@@ -6,8 +10,10 @@ const serializeArguments = (args) => {
    });
 };
 
-const getArgumentValue = (arg, args) => {
-   return args?.find((a) => a.name === arg)?.value;
+const getArgumentValue = (arg, args = []) => {
+   const value = args?.find((a) => a.name === arg)?.value;
+   if (!value) throw new Error('There is no value for the provided argument');
+   return value;
 };
 
 module.exports = {
--- utils.test.js
+++ utils.test.js
@@ -1,23 +1,39 @@
 const { getArgumentValue, serializeArguments } = require('./utils');
 
 describe('utils', () => {
-   it('should serialize arguments list', () => {
-      const args = ['--filter=ry', '--test=toto', '--toast'];
-      const serializedArgs = [
-         { name: 'filter', value: 'ry' },
-         { name: 'test', value: 'toto' },
-         { name: 'toast' },
-      ];
-      expect(serializeArguments(args)).toStrictEqual(serializedArgs);
+   describe('serializeArguments', () => {
+      it('should throw an error if arguments list is empty', () => {
+         const args = [];
+         expect(() => {
+            serializeArguments(args);
+         }).toThrow();
+      });
+      it('should serialize arguments list', () => {
+         const args = ['--filter=ry', '--test=toto', '--toast'];
+         const serializedArgs = [
+            { name: 'filter', value: 'ry' },
+            { name: 'test', value: 'toto' },
+            { name: 'toast' },
+         ];
+         expect(serializeArguments(args)).toStrictEqual(serializedArgs);
+      });
    });
 
-   it('should return the value of an argument', () => {
-      const args = [
-         { name: 'filter', value: 'ry' },
-         { name: 'test', value: 'toto' },
-         { name: 'toast', value: 'titi' },
-      ];
-      const value = 'ry';
-      expect(getArgumentValue('filter', args)).toBe(value);
+   describe('getArgumentValue', () => {
+      it('should throw an error if no value is provided for the argument', () => {
+         expect(() => {
+            getArgumentValue('noval');
+         }).toThrow();
+      });
+
+      it('should return the value of an argument', () => {
+         const args = [
+            { name: 'filter', value: 'ry' },
+            { name: 'test', value: 'toto' },
+            { name: 'toast', value: 'titi' },
+         ];
+         const value = 'ry';
+         expect(getArgumentValue('filter', args)).toBe(value);
+      });
    });
 });
